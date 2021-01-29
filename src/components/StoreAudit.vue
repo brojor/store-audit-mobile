0c7076be62b5300b38c372b1ef1c112a86407539
Bronislav Jordán <bronislav.jordan@gmail.com>
2021-01-29 17:55:08 +0100

    funkční prototyp swipování

--- src/components/StoreAudit.vue
+++ src/components/StoreAudit.vue
@@ -2,9 +2,14 @@
   <main>
     <h1>store audit</h1>
     <div v-for="(kategory, index) in table" :key="index" class="kategory">
-      <div>{{ kategory.name }}</div>
+      <div @click="method1(index)">{{ kategory.name }}</div>
       <div class="points" v-if="activeKategory === index">
         <div
+          ref="trgt"
+          @touchstart="touchstart"
+          @touchend="touchend(i, $event)"
+          @touchmove="touchmove(i, $event)"
+          draggable="true"
           class="point"
           v-for="(point, i) in kategory.points"
           :key="i"
@@ -29,7 +34,37 @@ export default {
     return {
       table,
       activeKategory: null,
+      x: {},
     };
+  },
+  methods: {
+    touchstart(event) {
+      this.x.start = event.touches[0].clientX;
+    },
+    touchend(index, event) {
+      this.x.end = event.changedTouches[0].clientX;
+
+      console.log(this.x.start - this.x.end);
+      this.$refs.trgt[index].style.transform = 'translate3d(0px, 0px, 0px)';
+    },
+    touchmove(index, event) {
+      const xNow = event.touches[0].clientX;
+      const move = xNow - this.x.start;
+      console.log(move);
+
+      this.$refs.trgt[
+        index
+      ].style.transform = `translate3d(${move}px, 0px, 0px)`;
+      if (move > 360 / 3) {
+        this.$refs.trgt[index].style.backgroundColor = 'green';
+      }
+      if (move < -360 / 3) {
+        this.$refs.trgt[index].style.backgroundColor = 'red';
+      }
+    },
+    method1(index) {
+      this.activeKategory = index;
+    },
   },
 };
 </script>
