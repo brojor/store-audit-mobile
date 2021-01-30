ec14c13bf051fb660cdf9e891f6b74f1d2cb5e98
Bronislav Jordán <bronislav.jordan@gmail.com>
2021-01-30 09:05:29 +0100

    po uvolnění posouvané položky (swipe) nyní plynule dojede zpět

--- src/components/StoreAudit.vue
+++ src/components/StoreAudit.vue
@@ -6,11 +6,11 @@
       <div class="points" v-if="activeKategory === index">
         <div
           ref="trgt"
-          @touchstart="touchstart"
+          @touchstart="touchstart(i, $event)"
           @touchend="touchend(i, $event)"
           @touchmove="touchmove(i, $event)"
           draggable="true"
-          class="point"
+          class="point transformSlow"
           v-for="(point, i) in kategory.points"
           :key="i"
           :point="point"
@@ -35,31 +35,34 @@ export default {
       table,
       activeKategory: null,
       x: {},
+      touchTarget: null,
     };
   },
   methods: {
-    touchstart(event) {
+    touchstart(index, event) {
       this.x.start = event.touches[0].clientX;
+      this.touchTarget = this.$refs.trgt[index];
+      this.touchTarget.classList.remove('transformSlow');
     },
     touchend(index, event) {
       this.x.end = event.changedTouches[0].clientX;
 
       console.log(this.x.start - this.x.end);
       this.$refs.trgt[index].style.transform = 'translate3d(0px, 0px, 0px)';
+      this.touchTarget.classList.add('transformSlow');
     },
     touchmove(index, event) {
+      const target = this.$refs.trgt[index];
       const xNow = event.touches[0].clientX;
       const move = xNow - this.x.start;
       console.log(move);
 
-      this.$refs.trgt[
-        index
-      ].style.transform = `translate3d(${move}px, 0px, 0px)`;
+      target.style.transform = `translate3d(${move}px, 0px, 0px)`;
       if (move > 360 / 3) {
         this.$refs.trgt[index].style.backgroundColor = 'green';
       }
       if (move < -360 / 3) {
-        this.$refs.trgt[index].style.backgroundColor = 'red';
+        target.style.backgroundColor = 'red';
       }
     },
     method1(index) {
@@ -80,5 +83,8 @@ export default {
   border-bottom: 1px solid black;
   padding: 0.5rem;
   background-color: gray;
+}
+.transformSlow {
+  transition: transform 0.5s;
 }
 </style>
