e3adc98088902232d95ab5b7210deb5150b3d612
Bronislav Jordán <bronislav.jordan@gmail.com>
2021-02-01 08:45:26 +0100

    refactor po výměně datového modelu

--- src/components/StoreAudit.vue
+++ src/components/StoreAudit.vue
@@ -2,66 +2,45 @@
   <main>
     <h1>Store audit</h1>
     <div
-      v-for="(kategory, key, katIndex) in table"
+      v-for="(katValue, katKey, katIndex) in table"
       :key="katIndex"
       class="kategory"
     >
       <div
-        @click="method1(katIndex)"
+        @click="dropDown(katIndex)"
         class="kategory-title"
         :class="{ active: activeKategory === katIndex }"
       >
-        {{ kategories[key] }}
+        {{ kategories[katKey] }}
       </div>
-      <div class="points" v-if="activeKategory === katIndex">
-        <div
-          v-for="(value2, key2, index2) in kategory"
-          :key="index2"
-          class="point transformSlow"
-          :class="value2.status"
-          ref="trgt"
-          @touchstart="touchstart(index2, $event)"
-          @touchend="touchend(index2, $event)"
-          @touchmove="touchmove(value2, key, key2, index2, $event)"
-          draggable="true"
-        >
-          <p>{{ points[key][key2] }}</p>
-          <!-- <p>{{ value2.status }}</p> -->
-        </div>
-      </div>
-
       <transition name="roll">
-        <!-- <div class="points" v-if="activeKategory === katIndex"> -->
-        <!--         <div
-          class="point transformSlow"
-          :class="point.status"
-          ref="trgt"
-          @touchstart="touchstart(pointIndex, $event)"
-          @touchend="touchend(pointIndex, $event)"
-          @touchmove="touchmove(katIndex, pointIndex, $event)"
-          draggable="true"
-          v-for="(point, pointIndex) in kategory"
-          :key="pointIndex"
-          :point="point"
-        >
-          {{ point }}
-        </div> -->
-        <!-- </div> -->
+        <div class="points" v-if="activeKategory === katIndex">
+          <div
+            v-for="(pointValue, pointKey, pointIndex) in katValue"
+            :key="pointIndex"
+            class="point transformSlow"
+            :class="pointValue.status"
+            ref="trgt"
+            @touchstart="touchstart(pointIndex, $event)"
+            @touchend="touchend"
+            @touchmove="touchmove(pointValue, katKey, pointKey, $event)"
+            draggable="true"
+          >
+            <p>{{ points[katKey][pointKey] }}</p>
+          </div>
+        </div>
       </transition>
+      <transition name="roll"> </transition>
     </div>
   </main>
 </template>
 
 <script>
-// import CategoryPoint from '@/components/CategoryPoint.vue';
-// import table from '../../data';
-// import dataStore from '../../dataStore';
 import table from '../../dataStore';
 import { kategories, points } from '../../names';
 
 export default {
   name: 'StoreAudit',
-  // components: { CategoryPoint },
 
   data() {
     return {
@@ -69,83 +48,49 @@ export default {
       points,
       emitet: '',
       table,
-      // dataStore,
       activeKategory: null,
-      x: {},
+      posX: {},
       touchTarget: null,
-      // kategoryTitles: {
-      //   kat0: 'Před prodejnou, vstup do prodejny',
-      //   kat1: 'V prodejně',
-      //   kat2: 'Sklad, zázemí, kancelář',
-      //   kat3: 'Layout, POS a MKT označení',
-      //   kat4: 'Prezentace',
-      //   kat5: 'Provoz',
-      //   kat6: 'Stav zásob, "díry"',
-      //   kat7: 'Doplněnost',
-      //   kat8: 'Ceny, označení zboží',
-      //   kat9: 'Expirace',
-      //   kat10: 'Personál',
-      // },
     };
   },
   methods: {
     touchstart(index, event) {
-      this.x.start = event.touches[0].clientX;
+      this.posX.start = event.touches[0].clientX;
       this.touchTarget = this.$refs.trgt[index];
       this.touchTarget.classList.remove('transformSlow');
     },
-    touchend(index, event) {
-      this.x.end = event.changedTouches[0].clientX;
-
-      // console.log(this.x.start - this.x.end);
-      this.$refs.trgt[index].style.transform = 'translate3d(0px, 0px, 0px)';
+    touchend() {
+      this.touchTarget.style.transform = 'translate3d(0px, 0px, 0px)';
       this.touchTarget.classList.add('transformSlow');
     },
-    touchmove(value2, key, key2, index2, event) {
-      // console.log({ katIndex }, { pointIndex }, { event });
-      const target = this.$refs.trgt[index2];
-      // console.log({ target });
-      const xNow = event.touches[0].clientX;
-      const move = xNow - this.x.start;
-      // console.log(move);
-      if (Math.abs(move) > 15) {
-        target.style.transform = `translate3d(${move}px, 0px, 0px)`;
+    touchmove(pointValue, katKey, pointKey, event) {
+      const posXNow = event.touches[0].clientX;
+      const move = posXNow - this.posX.start;
+      if (Math.abs(move) > 20) {
+        this.touchTarget.style.transform = `translate3d(${move}px, 0px, 0px)`;
       }
-      if (Math.abs(move) > 360 / 3) {
-        // const { status } = this.dataStore[`kat${katIndex}`][`p${pointIndex}`];
-        const { status } = value2;
+      if (Math.abs(move) > window.innerWidth / 3) {
+        const { status } = pointValue;
         const swipeDirection = move > 0 ? 'right' : 'left';
         if (status !== 'accept' && swipeDirection === 'right') {
-          // this.swipedRigth(katIndex, pointIndex);
-          this.table[key][key2].status = 'accept';
+          this.swipedRigth(katKey, pointKey);
         }
         if (status !== 'reject' && swipeDirection === 'left') {
-          // this.swipedLeft(katIndex, pointIndex);
-          this.table[key][key2].status = 'reject';
+          this.swipedLeft(katKey, pointKey);
         }
       }
     },
-    method1(index) {
+    dropDown(index) {
       this.activeKategory = this.activeKategory === index ? null : index;
     },
     // calcTotalValue(points) {
     //   return points.reduce((acc, val) => acc + val.value, 0);
     // },
-    swipedRigth(katIndex, pointIndex) {
-      console.log(katIndex, pointIndex);
-      // const target = this.$refs.trgt[pointIndex];
-      // target.style.backgroundColor = '#3DDC97';
-      // target.style.color = 'white';
-
-      // END nebude nutne - bude se řešit formátováním podle true/false v dataStoru
-
-      this.dataStore[`kat${katIndex}`][`p${pointIndex}`] = 'accept';
+    swipedRigth(katKey, pointKey) {
+      this.table[katKey][pointKey].status = 'accept';
     },
-    swipedLeft(katIndex, pointIndex) {
-      this.dataStore[`kat${katIndex}`][`p${pointIndex}`] = 'reject';
-      // const target = this.$refs.trgt[pointIndex];
-      // target.style.backgroundColor = '#f0544f';
-      // target.style.color = 'white';
+    swipedLeft(katKey, pointKey) {
+      this.table[katKey][pointKey].status = 'reject';
     },
   },
 };
